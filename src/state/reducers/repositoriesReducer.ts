f2385b97b265ee15743c6dc30ee126e3b398204c
isylla <isylla@adio.gov.ae>
2023-10-20 12:33:06 +0400

    add: action interface

--- src/state/reducers/repositoriesReducer.ts
+++ src/state/reducers/repositoriesReducer.ts
@@ -3,19 +3,31 @@ interface RepositoriesState {
   error: string | null;
   data: string[];
 }
-
-interface Action {
-  type: string;
-  payload?: any
+interface SearchRepositoriesAction {
+  type: "search_repositories";
+}
+interface SearchRepositoriesSuccessAction {
+  type: "search_repositories_success";
+  payload: string[];
+}
+interface SearchRepositoriesErrorAction {
+  type: "search_repositories_error";
+  payload: string;
 }
 
-const reducer = (state: RepositoriesState, action: Action): RepositoriesState => {
+const reducer = (
+  state: RepositoriesState,
+  action:
+    | SearchRepositoriesAction
+    | SearchRepositoriesSuccessAction
+    | SearchRepositoriesErrorAction
+): RepositoriesState => {
   switch (action.type) {
-    case 'search_repositories':
+    case "search_repositories":
       return { loading: true, error: null, data: [] };
-    case 'search_repositories_success':
+    case "search_repositories_success":
       return { loading: false, error: null, data: action.payload };
-    case 'search_repositories_error':
+    case "search_repositories_error":
       return { loading: false, error: action.payload, data: [] };
     default:
       return state;

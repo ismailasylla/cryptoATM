a4b187ec5029f9e6abaa2834b3b9a1c27cc154a1
isylla <isylla@adio.gov.ae>
2023-10-21 15:53:32 +0400

    add: request logic

--- src/state/action-creators/index.ts
+++ src/state/action-creators/index.ts
@@ -1,14 +1,31 @@
 import axios from "axios";
-import { Action } from "../actions";
 import { ActionType } from "../action-types";
+import { Action } from "../actions";
 
-const seachRepositories = (term: string) => {
+export const seachRepositories = (term: string) => {
   return async (dispatch: any) => {
     dispatch({
       type: ActionType.SEARCH_REPOSITORIES
     });
 
     try {
+      const { data } = await axios.get(
+        "https://registry.npmjs.org/-/v1/search",
+        {
+          params: {
+            text: term
+          }
+        }
+      );
+
+      const names = data.objects.map((result: any) => {
+        return result.package.name;
+      });
+
+      dispatch({
+        type: ActionType.SEARCH_REPOSITORIES_SUCCESS,
+        payload: names
+      });
     } catch (err: any) {
       dispatch({
         type: ActionType.SEARCH_REPOSITORIES_ERROR,

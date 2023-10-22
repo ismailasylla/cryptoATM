f530e22c4659e1352b76e3ede04c6ee80e1cc12f
isylla <isylla@adio.gov.ae>
2023-10-22 22:20:45 +0400

    add: submit event

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -1,8 +1,16 @@
+import { useState } from "react";
+
 const RepositoriesList: React.FC = () => {
+  const [term, setTerm] = useState("");
+
+  const onSubmit = (e: any) => {
+    e.preventDefault();
+  };
+
   return (
     <div>
-      <form>
-        <input type="text" />
+      <form onSubmit={onSubmit}>
+        <input value={term} onChange={(e) => setTerm(e.target.value)} />
         <button>Search</button>
       </form>
     </div>

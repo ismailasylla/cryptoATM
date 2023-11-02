8603b7631f61fd606b758e53db8f0647f906961c
isylla <isylla@adio.gov.ae>
2023-11-02 23:22:28 +0400

    implement: typed selector

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -1,9 +1,16 @@
 import { useState } from "react";
+import { useTypedSelector } from "../hooks/useTypedSelector";
 import { useActions } from "../hooks/useActions";
 
 const RepositoriesList: React.FC = () => {
   const [term, setTerm] = useState("");
   const { searchRepositories } = useActions();
+  const { data, error, loading } = useTypedSelector(
+    (state: any) => state.repositories
+  );
+  // const state = useSelector((state) => state);
+
+  console.log(data);
 
   const onSubmit = (e: any) => {
     e.preventDefault();
